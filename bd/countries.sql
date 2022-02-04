3d177711bd14f939baea28b4a6f4211cddc0076f
prokaeveg <prokaev96186903@gmail.com>
2022-02-04 16:55:51 +0300

    added bd

---
+++ bd/countries.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Фев 04 2022 г., 16:54
+-- Версия сервера: 10.3.22-MariaDB
+-- Версия PHP: 7.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `countries`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `countries`
+--
+
+CREATE TABLE `countries` (
+  `id` int(11) NOT NULL,
+  `country_name` varchar(255) NOT NULL,
+  `capital_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `countries`
+--
+
+INSERT INTO `countries` (`id`, `country_name`, `capital_name`) VALUES
+(1, 'Russia', 'Moscow'),
+(4, 'USA', 'Washington'),
+(5, 'Azerbaijan', 'Baku'),
+(7, '	Brazil', 'Brasilia'),
+(9, 'Chile', 'Santiago'),
+(10, 'Colombia', 'Bogota'),
+(11, 'Croatia', 'Zagreb'),
+(12, 'Dominican Republic', 'Santo Domingo'),
+(13, 'Haiti', 'Port-au-Prince');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `countries`
+--
+ALTER TABLE `countries`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `countries`
+--
+ALTER TABLE `countries`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
